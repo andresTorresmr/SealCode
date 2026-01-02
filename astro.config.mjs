505f818a4a04dd1ab09b3a060fa26d37f18724d6
andresTorresmr <andrestorresm001@gmail.com>
2026-01-02 17:42:50 -0600

    SealCode secciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://sealcode.mx",
+  site: "https://sealcode.com.mx",
   output: "server",
 
   vite: { plugins: [tailwindcss()] },
   adapter: vercel(),
+  integrations: [sitemap({
+    // filter: (page) => page !== "https://example.com/secret-vip-lounge/",
+  }), partytown()],
 });
